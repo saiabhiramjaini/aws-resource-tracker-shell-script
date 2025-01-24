e4ceb94654e0824f37a7fcbb420175534982a8f9
JAINI SAI ABHIRAM <115941546+saiabhiramjaini@users.noreply.github.com>
2025-01-24 23:46:46 +0530

    Add files via upload

---
+++ aws-resource-tracker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+####################################
+#
+# Author: Jaini Sai Abhiram
+# Date: 24/01/2025
+# 
+# Version: v1
+# 
+# This script will report AWS resources usage
+#
+####################################
+
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+set -x
+set -e
+set -o pipefail
+
+# list of s3 buckets
+echo "Print list of S3 buckets"
+aws s3 ls
+
+# list of EC2 instances
+echo "Print list of EC2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# list lambda
+echo "Print list of lambda functions"
+aws lambda list-functions
+
+# list IAM users
+echo "Print list of IAM users"
+aws iam list-users
+
+
+
